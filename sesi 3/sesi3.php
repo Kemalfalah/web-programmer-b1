2e3e4c294b100e99a6209ccf113777f22a9e9f18
Kemalfalah <falahkemalluthfi@gmail.com>
2023-03-05 00:34:33 +0700

    komentar

--- sesi 3/sesi3.php
+++ sesi 3/sesi3.php
@@ -12,8 +12,10 @@
     <div class="box"> Box 1</div>
     <div class="box_dua"> Box 2</div>
 
-    <h1 class="tulisan_satu"></h1>
-    <h1 class="tulisan_dua"></h1>
-    <h1 class="tulisan_tiga"></h1>
+    <h1 class="tulisan_satu">Belajar1 CSS dasar di Hacktiv8</h1>
+    <h1 class="tulisan_dua">Belajar2 CSS dasar di Hacktiv8</h1>
+    <h1 class="tulisan_tiga">Belajar3 CSS dasar di Hacktiv8</h1>
+
+    <a href="https://google.com" class="link" target="_blank">Klik disini</a>
 </body>
 </html>
